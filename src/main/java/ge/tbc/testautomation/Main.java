3d7497258faf9a3041ebdb252f8c3a9b420d8ed3
cotneaburjania@gmail.com <cotneaburjania@gmail.com>
2026-06-23 19:43:52 +0400

    Added someNewFunction

--- src/main/java/ge/tbc/testautomation/Main.java
+++ src/main/java/ge/tbc/testautomation/Main.java
@@ -3,5 +3,10 @@ package ge.tbc.testautomation;
 public class Main {
     public static void main(String[] args) {
         System.out.println("I AM THE MAIN FUNCTION");
+        someNewFunction();
+    }
+
+    private static void someNewFunction(){
+        System.out.println("I DO SOMETHING NEW");
     }
 }
