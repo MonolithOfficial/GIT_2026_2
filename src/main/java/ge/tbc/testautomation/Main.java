8ef857a8702c1ad0ab7ef01996de2284f580d5c0
cotneaburjania@gmail.com <cotneaburjania@gmail.com>
2026-06-23 20:12:32 +0400

    Added integer function

--- src/main/java/ge/tbc/testautomation/Main.java
+++ src/main/java/ge/tbc/testautomation/Main.java
@@ -3,10 +3,14 @@ package ge.tbc.testautomation;
 public class Main {
     public static void main(String[] args) {
         System.out.println("I AM THE MAIN FUNCTION");
-        someNewFunction();
+        int number = someIntegerFunction();
     }
 
     private static void someNewFunction(){
         System.out.println("I DO SOMETHING NEW");
+    }
+
+    private static int someIntegerFunction(){
+        return 1;
     }
 }
