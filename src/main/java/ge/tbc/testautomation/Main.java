15dd6684b545bb28540b6edb4c1a3ff596cc9025
cotneaburjania@gmail.com <cotneaburjania@gmail.com>
2026-06-23 19:45:42 +0400

    Added someNewFunction with string return type

--- src/main/java/ge/tbc/testautomation/Main.java
+++ src/main/java/ge/tbc/testautomation/Main.java
@@ -3,5 +3,10 @@ package ge.tbc.testautomation;
 public class Main {
     public static void main(String[] args) {
         System.out.println("I AM THE MAIN FUNCTION");
+        String result = someNewFunction("MESSAGE");
+    }
+
+    private static String someNewFunction(String message){
+        return message;
     }
 }
